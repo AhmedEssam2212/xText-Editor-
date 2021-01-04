bf58aad21c8de3b63102d4cb7ea5072bfbd5a9a1
AHMED-ESSAM <ahmed.essam@Hypercell.net>
2021-01-04 20:01:07 +0200

    create object from fnotePad

--- src/project/Project.java
+++ src/project/Project.java
@@ -3,7 +3,7 @@ package project;
 
 /**
  *
- * @author android
+ * @author Ahmed Essam
  */
 public class Project {
 
@@ -11,7 +11,7 @@ public class Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        fnotebad fn = new fnotebad();
     }
     
 }
